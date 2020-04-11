f270c52426c92b62bacf4b6b4724fce005f3832e
Chad Killingsworth <ckillingsworth@jackhenry.com>
2020-04-10 19:25:36 -0500

    Update configs for PKCE

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ module.exports = {
   client: {
     'silverlake-local': {
       client_id: '042bd8a9-f470-4804-baa2-4910df7de9cf', // These credentials are designed for *demonstration* purposes only.
-      client_secret: 'a85c2227-1198-4605-b2e5-326411aee27c', // These credentials are designed for *demonstration* purposes only.
+      // client_secret: 'a85c2227-1198-4605-b2e5-326411aee27c', // These credentials are designed for *demonstration* purposes only.
       grant_types: ['authorization_code'],
       response_types: ['code'],
-      // token_endpoint_auth_method: 'client_secret_basic',
+      token_endpoint_auth_method: 'none',
       redirect_uris: ['https://localhost:8080/auth/cb']
     }
   },
