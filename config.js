44bee9233c71a519053cfe16704e65964bf0d4a9
Jaime Lopez <31429468+DevWithTheHair@users.noreply.github.com>
2020-05-13 13:15:33 -0700

    fix!: switch to 2019silverlake environment (#13)
    * fix!: update URLs to 2019silverlake environment
    
    * fix!: update credentials 2019silverlake environment

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ module.exports = {
   issuer: {
     'silverlake-local': {
       issuer: 'https://www.banno.com/a/consumer/api/oidc',
-      authorization_endpoint: 'https://silverlake.banno-production.com/a/consumer/api/oidc/auth',
-      token_endpoint: 'https://silverlake.banno-production.com/a/consumer/api/oidc/token',
-      jwks_uri: 'https://silverlake.banno-production.com/a/consumer/api/oidc/certs'
+      authorization_endpoint: 'https://2019silverlake.banno-production.com/a/consumer/api/oidc/auth',
+      token_endpoint: 'https://2019silverlake.banno-production.com/a/consumer/api/oidc/token',
+      jwks_uri: 'https://2019silverlake.banno-production.com/a/consumer/api/oidc/certs'
     }
   },
   client: {
     'silverlake-local': {
-      client_id: '42b799f2-e543-4910-b5ec-e69fd458814b', // These credentials are designed for *demonstration* purposes only.
-      client_secret: 'd75b6fb3-91b8-4935-9251-651b294249de', // These credentials are designed for *demonstration* purposes only.
+      client_id: '55fc6a69-a4dd-404c-97f9-e2361b4c44b1', // These credentials are designed for *demonstration* purposes only.
+      client_secret: 'da9003cc-438a-4bbe-95d0-9af6ffe36d6a', // These credentials are designed for *demonstration* purposes only.
       grant_types: ['authorization_code'],
       response_types: ['code'],
       token_endpoint_auth_method: 'client_secret_basic',
@@ -18,7 +18,7 @@ module.exports = {
     }
   },
   consumerApi: {
-    environment: 'https://silverlake.banno-production.com',
+    environment: 'https://2019silverlake.banno-production.com',
     usersBase: '/a/consumer/api/v0/users/'
   }
 }
